251060e340e4b7dba53509a5acd32105cc023af6
danielarnarsson <danielar216@nemi.tskoli.is>
2017-12-02 22:54:30 +0000

    Created the database and product table

---
+++ Verkefnid/products.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE 1907002160_VEFTH2_lokaverkefni;
+USE 1907002160_VEFTH2_lokaverkefni;
+
+CREATE TABLE IF NOT EXISTS PRODUCTS (
+CATEGORY VARCHAR(100),
+TITLE VARCHAR(100),
+PRICE INT,
+INFO TEXT
+);
+
+#select * from products;
+#drop database 1907002160_vefth2_lokaverkefni;
